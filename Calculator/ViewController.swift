4dd51c997ca462b1d90de04f9714ffbf9dff8cf7
bhawkeswood <bhawkeswood@gmail.com>
2015-02-22 14:52:47 -0500

    Unwrapping optional

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var display: UILabel!
     
     @IBAction func appendDigit(sender: UIButton) {
-        let digit = sender.currentTitle
+        let digit = sender.currentTitle!
         println("digit = \(digit)")
     }
 }
