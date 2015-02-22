833b64266a8dba560c64001a08763b748e2e0ecb
bhawkeswood <bhawkeswood@gmail.com>
2015-02-22 14:59:18 -0500

    Updating logic to remove zero at front

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,9 +12,16 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var display: UILabel!
     
+    var userIsInTheMiddleOfTypingNumber: Bool = false
+    
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
-        println("digit = \(digit)")
+        if userIsInTheMiddleOfTypingNumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingNumber = true
+        }
     }
 }
 
