b2a7ccbd8dc833c1168f601eccb0c10f9f7fd821
bhawkeswood <bhawkeswood@gmail.com>
2015-02-22 16:31:05 -0500

    Wiring up operations

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var display: UILabel!
     
-    var userIsInTheMiddleOfTypingNumber: Bool = false
+    var userIsInTheMiddleOfTypingNumber = false
     
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
@@ -21,6 +21,61 @@ class ViewController: UIViewController {
         } else {
             display.text = digit
             userIsInTheMiddleOfTypingNumber = true
+        }
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingNumber {
+            enter()
+        }
+        switch operation {
+            case "×": performOperation(multiply)
+            case "÷": performOperation(divide)
+            case "+": performOperation(add)
+            case "−": performOperation(subtract)
+            default: break
+        }
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func multiply(op1: Double, op2: Double) -> Double {
+        return op1 * op2
+    }
+    
+    func divide(op1: Double, op2: Double) -> Double {
+        return op1 / op2
+    }
+    
+    func add(op1: Double, op2: Double) -> Double {
+        return op1 + op2
+    }
+    
+    func subtract(op1: Double, op2: Double) -> Double {
+        return op1 - op2
+    }
+
+    
+    var operandStack = Array<Double>()
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingNumber = false
+        operandStack.append(displayValue)
+        println("operandStack = \(operandStack)")
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        } set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingNumber = false
         }
     }
 }
